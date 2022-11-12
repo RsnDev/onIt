e0c0c37b2e71d6387136b790bf084622a48c8f26
Roushan <roushan0812@gmail.com>
2022-11-12 13:25:53 +0530

    modified:   .DS_Store 	new file:   assets/.DS_Store 	new file:   assets/image/bg.png 	modified:   utils/tabs/MyAccount.js

--- utils/tabs/MyAccount.js
+++ utils/tabs/MyAccount.js
@@ -1,50 +1,369 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { BottomSheet } from "react-native-btr";
-
-export default function MyAccount() {
-  const [visible, setVisible] = useState(false);
-
-  function toggle() {
-    setVisible((visible) => !visible);
-  }
-
+import React from "react";
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  ImageRequireSource,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
+const MyAccount = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={toggle}>
-        <View style={styles.button}>
-          <Text>Toggle BottomSheet</Text>
-        </View>
-      </TouchableOpacity>
-      <BottomSheet
-        visible={visible}
-        onBackButtonPress={toggle}
-        onBackdropPress={toggle}
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <ImageBackground
+        source={require("../../assets/image/bg.png")}
+        style={{ opacity: 50 }}
       >
-        <View style={styles.card}>
-          <Text>Place your custom view inside BottomSheet</Text>
+        <StatusBar
+          barStyle="dark-content"
+          hidden={false}
+          backgroundColor="#fff"
+        />
+
+        <View
+          style={{
+            flexDirection: "column",
+            //  justifyContent: "center",
+            height: 180,
+            alignItems: "center",
+            backgroundColor: "#00796A",
+            //opacity: 0.7,
+            marginBottom: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 21,
+              fontWeight: "700",
+              marginTop: 10,
+              color: "#fff",
+            }}
+          >
+            My Account
+          </Text>
+          <Image
+            source={require("../../assets/image/naruto.png")}
+            style={{
+              height: 110,
+              width: 110,
+              marginTop: 10,
+              marginLeft: 2,
+              borderRadius: 60,
+            }}
+          />
         </View>
-      </BottomSheet>
+
+        {/* for Booking */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              // backgroundColor: "#fff",
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+              borderRadius: 3,
+              //borderWidth: 1,
+              //borderColor: "#fff",
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              My Bookings
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* for Personal care */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              //backgroundColor: "#fff",
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+              // borderRadius: 3,
+              // borderWidth: 1,
+              // borderColor: "#fff",
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              Personal care
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* for scheduled booking */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              //backgroundColor: "#fff",
+              height: 60,
+
+              marginLeft: 15,
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              scheduled Bookings
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* for Manage Address */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              Manage Address
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Refer and earn */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              Refer & Earn
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* about company */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              About Us
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* about privacy policy */}
+        <TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+
+              height: 60,
+              //marginTop: 15,
+              marginLeft: 15,
+              //marginRight: 20,
+            }}
+          >
+            <Image
+              source={require("../../assets/logo/bonus.png")}
+              style={{
+                height: 25,
+                width: 25,
+                alignItems: "center",
+                marginLeft: 5,
+              }}
+            />
+            <Text
+              style={{
+                flex: 0.9,
+                fontWeight: "700",
+                fontSize: 17,
+                color: "black",
+                marginLeft: 20,
+              }}
+            >
+              privacy policy
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* for sign out */}
+        <View
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: "25%",
+              height: 40,
+              marginTop: 55,
+              marginBottom: 50,
+              marginLeft: 10,
+              borderRadius: 3,
+            }}
+            //</View>onPress={() => { console.log("coming soon") }}>
+            // onPress={() => {
+            //   alert("coming");
+            // }}
+          >
+            <Text
+              style={{
+                fontWeight: "900",
+                fontSize: 19,
+                letterSpacing: 1,
+                textAlign: "center",
+                position: "relative",
+                color: "#00796A",
+              }}
+            >
+              Sign Out
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#fff",
-    borderWidth: 2,
-    borderRadius: 50,
-    padding: 16,
-  },
-  card: {
-    backgroundColor: "#fff",
-    height: 250,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+export default MyAccount;
+
+const style = StyleSheet.create({});
